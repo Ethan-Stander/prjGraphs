74a9f0e6f2f6b889a6692c747a7f026b1e8db3fb
OofSpooder <spoodermankilledamerica@gmail.com>
2023-10-24 09:54:57 +0200

    Add project files.

---
+++ prjGraphs/CNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjGraphs
+{
+    public class CNode<T>
+    {
+        public int id; 
+        public LinkedList<CNode<T>> children = new LinkedList<CNode<T>>();
+        T data;
+
+        public CNode(int id, T data)
+        {
+            this.id = id;
+            this.data = data;
+        }
+    }
+}
---
+++ prjGraphs/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace prjGraphs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          CGraph cGraph = new CGraph();
+            cGraph.addNode(1, "Nicole");
+            cGraph.addNode(2, "Luke");
+            cGraph.addNode(3, "Stefan");
+
+         // cGraph.addEdges(1, 3);
+            cGraph.addEdges(2, 3);
+
+            bool yes = cGraph.hasPathDFS(1, 3);
+
+                if(yes)
+            {
+
+                Console.WriteLine("Nicole is related to Stefan, they can be cousins.");
+            }
+
+                else
+            {
+                Console.WriteLine("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO :)");
+            }
+
+            bool BFSYes = cGraph.hasPathDFS(2, 3);
+
+            if(BFSYes)
+            {
+                Console.WriteLine("BFS PATH FOUND between Luke and Stefan");
+            }
+
+            else
+            {
+                Console.WriteLine( "NOOOOOO");
+            }    
+
+
+
+
+
+
+        }
+    }
+}
